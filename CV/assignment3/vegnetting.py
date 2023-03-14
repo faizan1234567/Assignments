abe34218a086389e1581dc493b8ca431b10e3496
Faizan <engrfaizan.ai@gmail.com>
2023-03-14 13:10:42 +0500

    updated code

--- CV/assignment3/vegnetting.py
+++ CV/assignment3/vegnetting.py
@@ -10,7 +10,7 @@ def read_args():
     opt = parser.parse_args()
     return opt
 
-def remove_vegnetting_effect(veg_image, mask):
+def remove_vegnetting_effect(veg_image, mask, add_brightness = False):
     """remove vegnetting effect
     
     Parameters
@@ -23,17 +23,20 @@ def remove_vegnetting_effect(veg_image, mask):
     veg_image_copy = veg_image.copy()
     #remove the effect 
     for i in range(veg_image.shape[-1]):
-        veg_image_copy[:, :, i] =  veg_image_copy[:, :, i]/mask
+        veg_image_copy[:, :, i] =  veg_image_copy[:, :, i]//mask
+    output = veg_image_copy
+    output = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
     #enhance brighness and contrast
     #credit: https://stackoverflow.com/questions/62080675/remove-vignette-filter-of-colored-image
-    hsv = cv2.cvtColor(veg_image_copy, cv2.COLOR_BGR2HSV)
-    hsv = np.array(hsv, dtype = np.float64)
-    hsv[:,:,1] = hsv[:,:,1]*1.3 ## scale pixel values up or down for channel 1(Lightness)
-    hsv[:,:,1][hsv[:,:,1]>255]  = 255
-    hsv[:,:,2] = hsv[:,:,2]*1.3 ## scale pixel values up or down for channel 1(Lightness)
-    hsv[:,:,2][hsv[:,:,2]>255]  = 255
-    hsv = np.array(hsv, dtype = np.uint8)
-    output = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    if add_brightness:
+      hsv = cv2.cvtColor(veg_image_copy, cv2.COLOR_BGR2HSV)
+      hsv = np.array(hsv, dtype = np.float64)
+      hsv[:,:,1] = hsv[:,:,1]*1.3 ## scale pixel values up or down for channel 1(Lightness)
+      hsv[:,:,1][hsv[:,:,1]>255]  = 255
+      hsv[:,:,2] = hsv[:,:,2]*1.3 ## scale pixel values up or down for channel 1(Lightness)
+      hsv[:,:,2][hsv[:,:,2]>255]  = 255
+      hsv = np.array(hsv, dtype = np.uint8)
+      output = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return output
 
 def read_n_resize(image, size, RGB = False):
@@ -108,10 +111,10 @@ def main():
     if args.image:
         orig_image = read_n_resize(args.image, (640, 640), True)
         output_image, mask = apply_vegnetting(args.image)
-        removed_vegnetted_effect = remove_vegnetting_effect(output_image, mask)
+        removed_vegnetted_effect = remove_vegnetting_effect(output_image, mask, False)
         fig, axxr = plt.subplots(1, 3)
         titles = ["original", "Vegnetting effect", "removed effect"]
-        images = [orig_image, output_image, remove_vegnetting_effect]
+        images = [orig_image, output_image, removed_vegnetted_effect]
         for i in range(len(images)):
             axxr[i].imshow(images[i])
             axxr[i].set_title(titles[i])
