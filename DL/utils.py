02746ff1bc2844c2c1021ee1f6b408dada3da03d
MFaizan <mohammadfaizantanoli@gmail.com>
2023-10-07 11:27:27 +0500

    updated data normalization options

--- DL/utils.py
+++ DL/utils.py
@@ -100,15 +100,15 @@ def image_transforms(cfg: dict  = {},
         transforms = torchvision.transforms.Compose([
             transfroms.Resize(224), 
             transforms.ToTensor(),
-            transforms.Normalize(mean = [], 
-                                 std = [])
+            transforms.Normalize(mean = [0.485, 0.456, 0.406], 
+                                 std =  [0.229, 0.224, 0.225])
         ])
     else:
         transforms = torchvision.transforms.Compose([
             transforms.Resize(224),
             transforms.ToTesnor(),
-            transforms.Normalize(mean = [], 
-                                 std = [])
+            transforms.Normalize(mean = [0.485, 0.456, 0.406], 
+                                 std =  [0.229, 0.224, 0.225])
         ])
     return transforms
     
