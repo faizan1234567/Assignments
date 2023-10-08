40e2d19815ed5d9e76df27b6ba22213b3ed3020e
MFaizan <mohammadfaizantanoli@gmail.com>
2023-10-08 20:30:40 +0500

    updated code

--- DL/knn_scrath.py
+++ DL/knn_scrath.py
@@ -68,7 +68,7 @@ class KNNModel:
 
     def fit(self, X, y):
         self.X_train = X
-        self.y = y
+        self.y_train = y
 
     def euclidean_distance(self, img1, img2):
         return np.linalg.norm(img1 - img2)
@@ -88,6 +88,7 @@ class KNNModel:
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
         most_common = Counter(k_nearest_labels).most_common(1)
+        print(most_common[0][0])
         return most_common[0][0]
 
 if __name__ == "__main__":
@@ -108,6 +109,9 @@ if __name__ == "__main__":
     classifier = KNNModel(k = args.k, distance= 'Euclidean' if not args.manhatten else 'manhatten')
     classifier.fit(Xtrain, Ytrain)
     ytest_pred = classifier.predict(Xtest)
+    print(ytest_pred)
 
     # now calcualte the fucking accuracy
+    #TODO: TO TEST THE CODE
+
 
