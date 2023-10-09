bf3777b1231c311775c52fc58062ce493cbe9fce
unknown <mohammadfaizantanoli@gmail.com>
2023-10-09 10:42:01 +0500

    updated svm func

--- DL/svm_scratch.py
+++ DL/svm_scratch.py
@@ -87,6 +87,8 @@ class SVM:
     def predict(self, X):
         approx = np.dot(X, self.w) - self.b
         return np.sign(approx)
+
+        
 # TODO: slight updates to the code ..   
 if __name__ == "__main__":
     args = read_args()
